15fce9938cd54b4a86457f95f14934f3172b5236
mdnght <mdnght@sohu.com>
2020-02-27 10:59:48 +0800

    Update docker_nginx.sh

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -2,5 +2,29 @@
 #Docker 部署nginx
 #By author Baif
 
+Nginx_HOME=/data/local/nginx
+Nginx_Conf=/data/local/nginx/conf/nginx.conf
+Nginx_Logs=/data/logs/nginx
+Nginx_Dockername=Nginx-web
+Nginx_Port=80
 
-docker run -d -p 8081:80 --name nginx-web-test -v $PWD/nginx/www:/usr/share/nginx/html -v $PWD/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/nginx/logs:/var/log/nginx nginx
+function install_docker() {
+  #docker 安装
+  yum -y update
+  yum -y install python-devel
+  yum -y install docker-io
+  service docker start
+}
+
+if [[ ! -d ${Nginx_HOME} ]] ; then
+  mkdir -p ${Nginx_HOME}
+fi
+
+if [[ ! -d ${Nginx_Logs} ]] ; then
+  mkdir -p ${Nginx_Logs}  
+fi
+
+install_docker
+
+docker run -d -p ${Nginx_Port}:80 --name ${Nginx_Dockername} -v ${Nginx_Conf}:/etc/nginx/nginx.conf -v ${Nginx_Logs}:/var/log/nginx nginx
+
